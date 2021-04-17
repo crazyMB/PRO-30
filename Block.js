e2becdb662ca63bfe75903d13019ff0ba723c2ca
crazyMB <76734607+crazyMB@users.noreply.github.com>
2021-04-17 15:01:04 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,32 @@
+class Block{
+  constructor(x, y, width, height) {
+      var options = {
+          restitution :0.4,
+          friction :0.0,
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos= this.body.position;
+      var angle = this.body.angle;
+      if (this.body.speed<6) {
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        rect(0,0, this.width, this.height)
+        pop ();
+    } else {
+        push ();
+            World.remove(world,this.body);
+            this.visiblity = this.visiblity-5
+            tint(255,this.visiblity)
+        pop();
+        
+    }
+     
+    }
+}
